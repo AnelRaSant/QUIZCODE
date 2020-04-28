0e55f8de31f6227dd91967cb10f4e79e37647430
AnelRaSant <anel.rmrz.stgo7@gmail.com>
2020-04-28 10:26:07 -0500

    added new repository for hmw 4

---
+++ questions .js
@@ -0,0 +1,33 @@
+var questions = [
+    {
+        title: "My complete name is",
+        choices: ["<Anel Ramírez>", "<Anel Santiago>", "<Anel Ramírez Santiago>", "<Anel Ramírez Santillana>"],
+        answer: "<Anel Ramírez Santiago>"
+    },
+    {
+        title: "The year I was born is",
+        choices: ["1991", "1992", "1993", "1994"],
+        answer: "1993"
+    },
+    {
+        title: "From which city am I originally from?",
+        choices: ["<Mexico City>", "<Guadalajara>", "<Puebla>", "<Oaxaca>"],
+        answer: "<Oaxaca>"
+    },
+    {
+        title: "What kind of pet do I have?",
+        choices: ["cat", "dog", "baby pig", "fish"],
+        answer: "dog"
+    },
+    {
+        title: "What is my favorite hobbie?",
+        choices: ["read a book", "paint", "learn a new skill", "travel"],
+        answer: "travel"
+    },
+    {
+        title: "Am I feminist?",
+        choices: ["Yes", "No", "I have no clue", "Once in a while."],
+        answer: "Yes"
+    },
+];
+
